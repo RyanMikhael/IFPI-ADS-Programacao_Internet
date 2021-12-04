9498e43ab985a4330be1ef7ca692b2f0e16765e7
RyanMikhael <62043977+RyanMikhael@users.noreply.github.com>
2021-12-04 09:23:58 -0300

    Update and rename App.ts to Bad-SRP.ts

---
+++ Eng_Software/Atividade_03/SRP/Bad-SRP.ts
@@ -0,0 +1,17 @@
+class Pedido {
+    //atributos
+    id: number
+
+    // métodos
+    totalSum(){}
+    getItems(){}
+    getItemCount(){}
+    addItem(){}
+    deleteItem(){}
+
+    load(id){}
+    save(id){}
+    update(id){}
+    delete(id){}
+}
+
