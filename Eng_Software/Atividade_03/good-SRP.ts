3e2a5c22a15262b0d54e97425992d22d439317b1
RyanMikhael <62043977+RyanMikhael@users.noreply.github.com>
2021-12-04 09:30:49 -0300

    Create good-SRP.ts

---
+++ Eng_Software/Atividade_03/good-SRP.ts
@@ -0,0 +1,20 @@
+class Pedido {
+    //atributos
+    id: number
+
+    // métodos
+    totalSum(){}
+    getItems(){}
+    getItemCount(){}
+    addItem(){}
+    deleteItem(){}
+    
+}
+
+class repositorioPedidos{
+    load(id){}
+    save(id){}
+    update(id){}
+    delete(id){}
+
+}
