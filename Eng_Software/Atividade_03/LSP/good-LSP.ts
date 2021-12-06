db0b93b3b792ab0a4086c4f6b2ec9bb97f85cd8f
RyanMikhael <62043977+RyanMikhael@users.noreply.github.com>
2021-12-06 18:19:05 -0300

    Create good-LSP.ts

---
+++ Eng_Software/Atividade_03/LSP/good-LSP.ts
@@ -0,0 +1,19 @@
+class ave{
+    nome: string
+
+    bicar(): void{}
+}
+
+class beijaFlor extends ave{
+    bicar(): void{}
+    voar(): void{}
+}
+
+class pinguim extends ave{
+    bicar(): void{}
+}
+
+class avestruz extends ave{
+    bicar(): void{}
+    correrRapido(): void{}
+}
