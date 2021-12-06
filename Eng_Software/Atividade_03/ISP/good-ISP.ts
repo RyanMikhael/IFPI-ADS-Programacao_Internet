84db34497ad3b47e44e98617e787573cb744e6cd
RyanMikhael <62043977+RyanMikhael@users.noreply.github.com>
2021-12-06 18:36:57 -0300

    Update good-ISP.ts

--- Eng_Software/Atividade_03/ISP/good-ISP.ts
+++ Eng_Software/Atividade_03/ISP/good-ISP.ts
@@ -1,1 +1,45 @@
+interface automovel{
 
+    acelerar();
+    frear();
+
+}
+
+interface empinar{
+
+    empinarPneu();
+}
+
+interface viagemNoTempo{
+
+    viajarNoTempo();
+}
+
+
+class ford implements automovel{
+    acelerar(): void{}
+    frear(): void{}
+
+}
+
+class fiat implements automovel{
+    acelerar(): void{}
+    frear(): void{}
+
+}
+
+class yamaha implements automovel, empinar{
+    acelerar(): void{}
+    frear(): void{}
+    empinarPneu(): void{}
+
+
+}
+
+class deLorean implements automovel, viagemNoTempo{
+    acelerar(): void{}
+    frear(): void{}
+
+    viajarNoTempo(): void{}
+
+}
