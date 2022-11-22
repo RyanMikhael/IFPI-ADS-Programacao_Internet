d28e69dd869aa8999860a0da41ccedf667e524f6
RyanMikhael <62043977+RyanMikhael@users.noreply.github.com>
2022-11-22 20:02:20 -0300

    Add files via upload

---
+++ ReducerEContext/src/App.tsx
@@ -0,0 +1,109 @@
+import { useState, useReducer } from 'react';
+import './App.css';
+import { AddTask } from './components/add_task';
+import { TaskList } from './components/list_task';
+import { TasksContext, TasksDispatchContext } from './components/task_context';
+
+
+export default function App() {
+  
+  const [tasks,dispatch] = useReducer(taskReducer, initialTasks);
+
+
+  function handleAddTask(text: string) {
+
+    dispatch({
+      type: 'added',
+      id: nextId++,
+      text: text,
+    });
+    
+  }
+
+  function handleChangeTask(task: Task) {
+    dispatch({
+      type: 'changed',
+      task: task,
+    });
+  
+  }
+
+  function handleDeleteTask(taskId: number) {
+    dispatch({
+      type: 'deleted',
+      taskId: taskId
+
+    })
+  }
+
+  
+  return(
+    <TasksContext.Provider value={tasks}>
+      <TasksDispatchContext.Provider value={dispatch}>
+
+        <div className="App">
+          <h1>React + Reducer</h1>
+          <h3>Tasks</h3>
+          
+          <AddTask onAddTask={handleAddTask} />
+          
+          <TaskList
+            tasks={tasks}
+            onChangeTask={handleChangeTask}
+            onDeleteTask={handleDeleteTask}
+            />
+          
+        </div>
+      </TasksDispatchContext.Provider>
+    </TasksContext.Provider>
+  )
+}
+
+function taskReducer(tasks: Task[] , action: any ){
+
+  if(action.type === 'added'){
+    return [
+      ...tasks,
+      {
+        id: action.id,
+        text: action.text,
+        done: false,
+      },
+    ];
+  }
+
+  else if( action.type === 'changed'){
+    return tasks.map((t) => {
+      if (t.id === action.task.id) {
+        return action.task;
+      } else {
+        return t;
+      }
+    });
+  }
+
+  else if( action.type === 'deleted'){
+
+    return tasks.filter((t) => t.id !== action.taskId)
+
+  }
+
+  else{
+    throw Error('Unknown action: ' + action.type);
+    
+  }
+
+}
+export interface Task{
+  id: number
+  text: string
+  done: boolean
+}
+
+let nextId = 3;
+
+const initialTasks: Task[] = [
+  {id: 0, text: 'Elaborar Aulas', done: true},
+  {id: 1, text: 'Estudar Flutter - Estados', done: false},
+  {id: 2, text: 'Correr avenida Raul Lopres', done: false},
+];
---
+++ ReducerEContext/src/components/add_task.tsx
@@ -0,0 +1,35 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+
+interface AddTaskProps{
+    onAddTask: (text: string) => void
+}
+
+export function AddTask({onAddTask}: AddTaskProps){
+
+    const [taskText, setTaskText] = useState('')
+
+    const handlerDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setTaskText(event.target.value)
+    }
+
+    const handlerSubmit = (event: FormEvent) => {
+        event.preventDefault()
+        onAddTask(taskText)
+        setTaskText('')
+    }
+
+    return (
+        <>
+            <form onSubmit={handlerSubmit}>
+                <input 
+                    type="text" 
+                    value={taskText} 
+                    onChange={handlerDescriptionChange}  
+                    placeholder="Descrição"
+                    id="input" />
+                    
+                <input type="submit" value="Adicionar Tarefa" id="button"/>
+            </form>
+        </>
+    )
+}
